a2c4be68411d9d6e9f3b34ea84ac86e1311c53af
Brendis21 <bx.millan.e@edu.utc.mx>
2022-05-25 12:32:23 -0500

    EjemploCondicional

---
+++ 01 Condiciones/EjemploCondion.cpp
@@ -0,0 +1,85 @@
+
+#include<iostream>
+
+using namespace std; 
+ 
+main(){
+
+ 	int edad, diaA, mesA, anioA;
+ 	int dia, mes, anio, anioR;
+ 	int mesR;
+	int diaR;
+	int diasdelmes;
+	
+	
+ 	cout<<"Ingresa los siguientes datos para calcular tu edad \n";
+	cout<<"Dia actual \n";
+ 	cin>>diaA;
+ 	cout<<"Año actual \n";
+ 	cin>>anioA;
+ 	
+ 	cout<<"Dia de nacimiento \n";
+ 	cin>>dia;
+ 	cout<<"Año de nacimiento \n";
+ 	cin>>anio;
+ 	
+ 	cout<<"Seleccione el mes actual \n ";
+ 	cout<<"1 = Enero \n";
+ 	cout<<"2 = Febreo \n";
+ 	cout<<"3 = Marzo \n";
+ 	cout<<"4 = Abril \n";
+ 	cout<<"5 = Mayo \n";
+ 	cout<<"6 = Junio \n";
+ 	cout<<"7 = Julio \n";
+ 	cout<<"8 = Agosto \n";
+ 	cout<<"9 = Septiembre \n";
+ 	cout<<"10 = Octubre \n";
+ 	cout<<"11 = Noviembre \n";
+ 	cout<<"12 = Diciembre \n";
+do {
+	cin >>mesA;
+if (mesA<1 || mesA>12)
+cout<<"Numero incorrecto, intente de nuevo";
+}
+while (mesA<1 || mesA>12);
+ 	cout<<"Seleccione el mes de nacimiento \n";
+ 	cout<<"1 = Enero \n";
+ 	cout<<"2 = Febreo \n";
+ 	cout<<"3 = Marzo \n";
+ 	cout<<"4 = Abril \n";
+ 	cout<<"5 = Mayo \n";
+ 	cout<<"6 = Junio \n";
+ 	cout<<"7 = Julio \n";
+ 	cout<<"8 = Agosto \n";
+ 	cout<<"9 = Septiembre \n";
+ 	cout<<"10 = Octubre \n";
+ 	cout<<"11 = Noviembre \n";
+ 	cout<<"12 = Diciembre \n";
+
+
+	cin>>mes;
+if(mes<1||mes>12){
+	cout<<"Numero incorrecto, intente de nuevo";
+}
+
+anioR = anioA - anio;
+mesR = mesA - mes;
+diaR = diaA - dia;
+
+if(diaR<0){
+	diaR= diaR + diasdelmes;
+	mesR = mesR - 1;
+}
+
+if(mesR<0){
+	mesR = mesR + 12;
+	anioR= anioR-1;
+}
+cout<<"Usted tiene \n";
+cout << "Años: " << anioR<< endl;
+cout << "Dias: " << diaR << endl;
+cout << "Mese: " << mes << endl;
+}
+
+
+
